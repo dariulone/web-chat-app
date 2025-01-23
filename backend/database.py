a25d09cf91b434a4b5cb3a08ecbdb38ccef34695
dariulone <dariulone@gmail.com>
2025-01-23 13:32:02 +0300

    0.0.1

--- backend/database.py
+++ backend/database.py
@@ -4,15 +4,19 @@ from settings.config import settings
 from settings.logging_config import logger
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+import ssl
 
 # Асинхронный URL базы данных PostgreSQL
 DATABASE_URL = settings.DB_URL  # Например: postgresql+asyncpg://user:password@localhost/dbname
-
+# Создание SSL-контекста для игнорирования проверки сертификата
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False  # Не проверять имя хоста
+ssl_context.verify_mode = ssl.CERT_NONE  # Отключить проверку сертификатов
 # Создание асинхронного подключения
 engine = create_async_engine(
     DATABASE_URL,
     connect_args={
-            "ssl": True  # Указывает, что нужно использовать SSL
+            "ssl": ssl_context  # Указывает, что нужно использовать SSL
         },
     pool_size=20,  # Размер пула соединений
     max_overflow=30,  # Максимальное количество переполнений
