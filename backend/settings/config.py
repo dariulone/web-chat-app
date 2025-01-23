622a0efc05cff182ee444129d2e1cc43e98ad8c7
dariulone <dariulone@gmail.com>
2025-01-23 14:25:03 +0300

    0.0.1

--- backend/settings/config.py
+++ backend/settings/config.py
@@ -17,8 +17,15 @@ class Settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     DB_URL: str
-    ALLOWED_ORIGIN: str
+    ALLOWED_ORIGIN: list[str]
     REDIS_URL: str
+
+    @property
+    def ALLOWED_ORIGIN_LIST(self) -> list[str]:
+        """Преобразует ALLOWED_ORIGIN из строки в список (если это строка)."""
+        if isinstance(self.ALLOWED_ORIGIN, str):
+            return self.ALLOWED_ORIGIN.split(",")
+        return self.ALLOWED_ORIGIN
 
     model_config = SettingsConfigDict(env_file=".env")
 
